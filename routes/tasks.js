4ce79e449ba8ee5231e092e3dc64f3e540e5101a
Ashik <ashik@disystem.net>
2020-10-27 12:18:34 +0600

    task endpoint added

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,242 +5,288 @@ const today = new Date().toISOString().slice(0, 19).replace("T", " ");
 const empty = "";
 let helper = require("../Helper/helper");
 
-// // Registration new Deal
-// router.post("/create_task", function (req, res) {
-//   let task_title = req.body.task_title;
-//   let task_type = req.body.task_type;
-//   let task_date = req.body.task_date;
-//   let queue_code = req.body.queue_code;
-//   let task_owner = req.body.task_owner;
+// Task insert
+router.post("/create_task", function (req, res) {
+  let task_title = req.body.task_title;
+  let task_type = req.body.task_type;
+  let task_date = req.body.task_date;
+  let queue_code = req.body.queue_code;
+  let task_owner = req.body.task_owner;
+  let task_code = helper.makeid(15);
 
+  sql =
+    "INSERT INTO tasks (task_code, task_title, task_type, task_date, queue_code,task_owner,status, create_date,update_date ) VALUES ('" +
+    task_code +
+    "', '" +
+    task_title +
+    "','" +
+    task_type +
+    "','" +
+    task_date +
+    "','" +
+    queue_code +
+    "','" +
+    task_owner +
+    "','" +
+    "pending" +
+    "','" +
+    today +
+    "','" +
+    today +
+    "')";
 
-//   let task_code = helper.makeid(15);
+  db.query(sql, function (err, result) {
+    if (!err) {
+      sql2 =
+        "INSERT INTO tasks_details (task_code, link_code, link_type,create_date,update_date ) VALUES ('" +
+        task_code +
+        "', '" +
+        task_owner +
+        "','" +
+        "owner" +
+        "','" +
+        today +
+        "','" +
+        today +
+        "')";
 
-//   sql =
-//     "INSERT INTO tasks (task_code, task_title, task_type, task_date, queue_code,task_owner, create_date,update_date ) VALUES ('" +
-//     task_code +
-//     "', '" +
-//     task_title +
-//     "','" +
-//     task_type +
-//     "','" +
-//     task_date +
-//     "','" +
-//     queue_code +
-//     "','" +
-//     task_owner +
-//     "','" +
-//     today +
-//     "','" +
-//     today +
-//     "')";
+      db.query(sql2, function (err, result) {
+        if (!err) {
+          res.send({
+            result: true,
+            task_code: task_code,
+            msg: "Task Inserted successfully",
+          });
+        } else {
+          res.send({
+            result: false,
+            msg: "Task insertion failed",
+            error: err,
+          });
+        }
+      });
+    } else {
+      res.send({
+        result: false,
+        msg: "Task creation failed",
+        error: err,
+      });
+    }
+  });
+});
 
-//   db.query(sql, function (err, result) {
-//     if (!err) {
-//       sql2 =
-//         "INSERT INTO deal_assignment (com_code, deal_code, assigned_by, assigned_to, assignment_serial,create_date ) VALUES ('" +
-//         com_code +
-//         "', '" +
-//         deal_code +
-//         "','" +
-//         deal_owner +
-//         "','" +
-//         deal_owner +
-//         "','" +
-//         "1" +
-//         "','" +
-//         today +
-//         "')";
+// Get All Task
+router.get("/tasks", (req, res) => {
+  db.query("SELECT * FROM tasks", (err, rows, fields) => {
+    if (!err) {
+      res.send({
+        result: true,
+        msg: "Tasks Found",
+        data: rows,
+      });
+    } else {
+      res.send({
+        result: false,
+        msg: "Sorry no data found",
+        error: err,
+      });
+    }
+  });
+});
 
-//       db.query(sql2, function (err, result) {
-//         if (!err) {
-//           res.send({
-//             result: true,
-//             deal_code: deal_code,
-//             msg: "Deal Assigned successfully",
-//           });
-//         } else {
-//           res.send({
-//             result: false,
-//             msg: "Deal assign failed",
-//             error: err,
-//           });
-//         }
-//       });
-//     } else {
-//       res.send({
-//         result: false,
-//         msg: "Deal creation failed",
-//         error: err,
-//       });
-//     }
-//   });
-// });
+// Get all assigned deals
+router.post("/task_list", (req, res) => {
+  //Inner join Examples
+  // https://www.mysqltutorial.org/mysql-inner-join.aspx/
+  db.query(
+    "SELECT * FROM tasks INNER JOIN tasks_details USING (task_code) WHERE link_code = ?",
+    [req.body.link_code],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send({
+          result: true,
+          msg: "Tasks Found",
+          data: rows,
+        });
+      } else {
+        res.send({
+          result: false,
+          msg: "No data found",
+          error: err,
+        });
+      }
+    }
+  );
+});
 
-// // Get All Users
-// router.get("/all_deals", (req, res) => {
-//   db.query("SELECT * FROM deal_info", (err, rows, fields) => {
-//     if (!err) {
-//       res.send({
-//         result: true,
-//         msg: "Deal Found",
-//         data: rows,
-//       });
-//     } else {
-//       res.send({
-//         result: false,
-//         msg: "Sorry something went wrong",
-//         error: err,
-//       });
-//     }
-//   });
-// });
+// Get all assigned deals
+router.post("/task_user_list", (req, res) => {
+  //Inner join Examples
+  // https://www.mysqltutorial.org/mysql-inner-join.aspx/
+  db.query(
+    "SELECT * FROM tasks_details INNER JOIN tasks USING (task_code) WHERE task_code = ?",
+    [req.body.task_code],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send({
+          result: true,
+          msg: "Tasks Found",
+          data: rows,
+        });
+      } else {
+        res.send({
+          result: false,
+          msg: "No data found",
+          error: err,
+        });
+      }
+    }
+  );
+});
 
-// // Get User Details
-// router.get("/deal_details", (req, res) => {
-//   db.query(
-//     "SELECT * FROM deal_info WHERE deal_code = ?",
-//     [req.body.deal_code],
-//     (err, rows, fields) => {
-//       if (!err) {
-//         res.send({
-//           result: true,
-//           msg: "Deal Found",
-//           data: rows,
-//         });
-//       } else {
-//         res.send({
-//           result: false,
-//           msg: "Sorry something went wrong",
-//           error: err,
-//         });
-//       }
-//     }
-//   );
-// });
+// Get User Details
+router.post("/task_details", (req, res) => {
+  db.query(
+    "SELECT * FROM tasks WHERE task_code = ?",
+    [req.body.task_code],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send({
+          result: true,
+          msg: "Tasks Found",
+          data: rows[0],
+        });
+      } else {
+        res.send({
+          result: false,
+          msg: "No task found",
+          error: err,
+        });
+      }
+    }
+  );
+});
 
-// // Get User Details
-// router.get("/deal_list", (req, res) => {
-//   db.query(
-//     "SELECT * FROM deal_info WHERE deal_owner = ?",
-//     [req.body.deal_owner],
-//     (err, rows, fields) => {
-//       if (!err) {
-//         res.send({
-//           result: true,
-//           msg: "Deals Found",
-//           data: rows,
-//         });
-//       } else {
-//         res.send({
-//           result: false,
-//           msg: "Sorry something went wrong",
-//           error: err,
-//         });
-//       }
-//     }
-//   );
-// });
+// Get User Details
+router.post("/task_list_user", (req, res) => {
+  db.query(
+    "SELECT * FROM tasks WHERE task_owner = ?",
+    [req.body.task_owner],
+    (err, rows, fields) => {
+      if (!err) {
+        res.send({
+          result: true,
+          msg: "Tasks Found",
+          data: rows,
+        });
+      } else {
+        res.send({
+          result: false,
+          msg: "No task found",
+          error: err,
+        });
+      }
+    }
+  );
+});
 
-// // Update userNAme
-// router.put("/deal_update", function (req, res) {
-//   let deal_name = req.body.deal_name;
-//   let deal_amount = req.body.deal_amount;
-//   let deal_stage = req.body.deal_stage;
-//   let deal_close_date = req.body.deal_close_date;
-//   let deal_progress = req.body.deal_progress;
-//   let deal_type = req.body.deal_type;
-//   let deal_code = req.body.deal_code;
+// Update Task Status
+router.put("/task_status_update", function (req, res) {
+  let task_code = req.body.task_code;
+  let status = req.body.status;
+  var sql =
+    "UPDATE tasks SET status = '" +
+    status +
+    "',update_date = '" +
+    today +
+    "' WHERE task_code = '" +
+    task_code +
+    "'";
+  db.query(sql, function (err, result) {
+    if (!err) {
+      res.send({
+        result: true,
+        msg: "Task status updated successfully",
+      });
+    } else {
+      res.send({
+        result: false,
+        msg: "Task status update Failed",
+        error: err,
+      });
+    }
+  });
+});
 
-//   var sql =
-//     "UPDATE deal_info SET deal_name = '" +
-//     deal_name +
-//     "',deal_amount = '" +
-//     deal_amount +
-//     "',deal_stage = '" +
-//     deal_stage +
-//     "',deal_close_date = '" +
-//     deal_close_date +
-//     "',deal_progress = '" +
-//     deal_progress +
-//     "',deal_type = '" +
-//     deal_type +
-//     "',update_date = '" +
-//     today +
-//     "' WHERE deal_code = '" +
-//     deal_code +
-//     "'";
-//   db.query(sql, function (err, result) {
-//     if (!err) {
-//       res.send({
-//         result: true,
-//         msg: "Deal Details Updated successfully",
-//       });
-//     } else {
-//       res.send({
-//         result: false,
-//         msg: "Deal Details Update Failed",
-//         error: err,
-//       });
-//     }
-//   });
-// });
+// Delete Task
+router.delete("/delete_task", function (req, res) {
+  let task_code = req.body.task_code;
+  let task_owner = req.body.task_owner;
 
-// // Update Deal Progresss
-// router.put("/deal_progress_update", function (req, res) {
-//   let deal_progress = req.body.deal_progress;
-//   let deal_code = req.body.deal_code;
-//   var sql =
-//     "UPDATE deal_info SET deal_progress = '" +
-//     deal_progress +
-//     "',update_date = '" +
-//     today +
-//     "' WHERE deal_code = '" +
-//     deal_code +
-//     "'";
-//   db.query(sql, function (err, result) {
-//     if (!err) {
-//       res.send({
-//         result: true,
-//         msg: "Deal Progress Updated successfully",
-//       });
-//     } else {
-//       res.send({
-//         result: false,
-//         msg: "Deal Progress Update Failed",
-//         error: err,
-//       });
-//     }
-//   });
-// });
+  mQuery =
+    "DELETE FROM tasks WHERE task_code = '" +
+    task_code +
+    "' AND task_owner = '" +
+    task_owner +
+    "' ";
 
-// // Update Deal stage
-// router.put("/deal_stage_update", function (req, res) {
-//   let deal_stage = req.body.deal_stage;
-//   let deal_code = req.body.deal_code;
-//   var sql =
-//     "UPDATE deal_info SET deal_stage = '" +
-//     deal_stage +
-//     "',update_date = '" +
-//     today +
-//     "' WHERE deal_code = '" +
-//     deal_code +
-//     "'";
-//   db.query(sql, function (err, result) {
-//     if (!err) {
-//       res.send({
-//         result: true,
-//         msg: "Deal Stage Updated Successfully",
-//       });
-//     } else {
-//       res.send({
-//         result: false,
-//         msg: "Deal Stage Update Failed",
-//         error: err,
-//       });
-//     }
-//   });
-// });
+  db.query(mQuery, function (err, result) {
+    if (!err) {
+      if (result["affectedRows"] > 0 ) {
+        res.send({
+          result: true,
+          msg: "Task deleted successfully",
+          res: result,
+        });
+      } else {
+        res.send({
+          result: false,
+          msg: "Only task creator can delete task",
+          res: result,
+        });
+      }
+    } else {
+      res.send({
+        result: false,
+        msg: "Task deletion failed",
+      });
+    }
+  });
+});
+
+// Task insert
+router.post("/task_details_add", function (req, res) {
+  let task_code = req.body.task_code;
+  let link_code = req.body.link_code;
+  let link_type = req.body.link_type;
+
+  sql =
+    "INSERT INTO tasks_details (task_code, link_code, link_type,create_date,update_date ) VALUES ('" +
+    task_code +
+    "', '" +
+    link_code +
+    "','" +
+    link_type +
+    "','" +
+    today +
+    "','" +
+    today +
+    "')";
+  db.query(sql, function (err, result) {
+    if (!err) {
+      res.send({
+        result: true,
+        msg: "Task assigned successfully",
+      });
+    } else {
+      res.send({
+        result: false,
+        msg: "Task assign failed",
+        error: err,
+      });
+    }
+  });
+});
 
 
 
